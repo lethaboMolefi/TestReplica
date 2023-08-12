bda6a413e201cc451f3adc4af95bfa9c09cc78e8
Mantjane <130645950+Mantjane@users.noreply.github.com>
2023-08-12 22:02:03 +0200

    Add files via upload
    MessageApp class will contain the main method to run using the JAR file of MessageStuff Frame

---
+++ MessageApp/src/messageapp/MessageApp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package messageapp;
+import za.ac.tut.gui.MessageStuff;
+/**
+ *
+ * @author TUMI
+ */
+public class MessageApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MessageStuff stuff;
+        stuff=new MessageStuff();
+    }
+    
+}
